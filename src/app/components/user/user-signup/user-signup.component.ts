4276101a3d9661f331a86145f26751f2da10fa1d
HassaanYounas <hassaanyounas123@gmail.com>
2020-04-15 22:38:03 +0500

    Another.

--- src/app/components/user/user-signup/user-signup.component.ts
+++ src/app/components/user/user-signup/user-signup.component.ts
@@ -12,6 +12,14 @@ import { UserAuthService } from '../../../services/user-auth.service';
 })
 export class UserSignupComponent {
 
+  validFirstName: boolean = true;
+  validLastName: boolean = true;
+  validEmail: boolean = true;
+  validPassword: boolean = true;
+  validPasswordConfirm: boolean = true;
+  validDOB: boolean = true;
+  validCellular: boolean = true;
+
   signUpForm: any;
   dialCodes: any;
   countryCode: string;
@@ -43,17 +51,17 @@ export class UserSignupComponent {
   }
 
   onSubmit(userData: any): void {
-    if (this.inputValidation.isString(userData.firstName) && this.inputValidation.isString(userData.lastName)) {
-      if (this.inputValidation.isEmail(userData.email)) {
-        if (userData.password.length >= 8) {
-          if (userData.password === userData.confirmPassword) {
-            if (this.countryCode !== 'Code') {
-              this.user.setValues(userData, this.countryCode);
-              this.userAuth.register(this.user);
-            }
-          }
-        }
-      }
-    }     
-  }  
+    this.validFirstName = !this.inputValidation.isString(userData.firstName) ? false : true;
+    this.validLastName = !this.inputValidation.isString(userData.lastName) ? false : true;
+    this.validEmail = !this.inputValidation.isEmail(userData.email) ? false : true;
+    this.validPassword = !(userData.password.length >= 8) ? false : true;
+    this.validPasswordConfirm = !(userData.password === userData.confirmPassword) ? false : true;
+    this.validDOB = userData.dateOfBirth === '' ? false : true;
+    this.validCellular = !(this.countryCode !== 'Code' && this.inputValidation.isPhoneNumber(userData.cellularNumber)) ? false : true;
+
+    if (this.validFirstName && this.validLastName && this.validEmail && this.validPassword && this.validPasswordConfirm && this.validDOB && this.validCellular) {
+      this.user.setValues(userData, this.countryCode);
+      this.userAuth.register(this.user);
+    }
+  }
 }
