373a4fd0b53c41d4d86a7822a5179770108259bb
Hassaan Younas <hassaanyounas123@gmail.com>
2020-06-16 17:57:06 +0500

    New Footer.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -17,6 +17,10 @@ export class AppComponent {
       .subscribe((event: NavigationEnd) => {
         this.current = event.url;
       });
+    this.router.events.subscribe((event) => {
+        if (!(event instanceof NavigationEnd)) return;
+        window.scrollTo(0, 0)
+    });
   }
 
   setActive(regexStr: string): string {
